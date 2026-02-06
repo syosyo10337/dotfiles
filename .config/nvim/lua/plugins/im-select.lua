a1aae3f2f125fb71ed6491a6e854013b7fcdc919
syosyo10337 <syosyo.ds@gmail.com>
2026-02-07 01:53:00 +0900

    chore:update lazy setup

--- .config/nvim/lua/plugins/im-select.lua
+++ .config/nvim/lua/plugins/im-select.lua
@@ -3,7 +3,8 @@ return {
     "keaising/im-select.nvim",
     config = function()
       require("im_select").setup({
-        default_im_select = "com.apple.keylayout.ABC",
+        default_command = "/opt/homebrew/bin/im-select",
+        default_im_select = "com.apple.keylayout.US",
         set_previous_events = { "InsertEnter" },
       })
     end,
